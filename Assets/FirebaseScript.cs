64d8145869358ebec7ee0880a2016d1a5209953b
Adarsh S <4darshofficial@gmail.com>
2018-02-24 17:06:40 +0530

    Fancy menu part -1

---
+++ Assets/FirebaseScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine.SceneManagement;
+
+
+public class FirebaseScript : MonoBehaviour {
+	public InputField Email, Password;
+
+	public void LoginButtonPressed()
+	{
+		FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync (Email.text, Password.text).
+		ContinueWith ((obj) => {
+			SceneManager.LoadSceneAsync ("LoggedInScene");
+		});
+	}
+
+	public void LoginAnonymousButtonPressed()
+	{
+		FirebaseAuth.DefaultInstance.SignInAnonymouslyAsync().
+		ContinueWith ((obj) => {
+			SceneManager.LoadSceneAsync ("LoggedInAnonScene");
+		});
+	}
+
+	public void CreateNewUserButtonPressed()
+	{
+		FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync (Email.text, Password.text).
+		ContinueWith ((obj) => {
+			SceneManager.LoadSceneAsync ("LoggedInScene");
+		});
+	}
+}	
